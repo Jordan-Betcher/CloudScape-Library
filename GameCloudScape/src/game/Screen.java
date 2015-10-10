8a11f5c5bf3c401dd3d9522ecfec92c35a76d38f
pplluumm <bunnydolphins_6@yahoo.com>
2015-10-10 07:48:40 -0700

    Created the Game Screen

---
+++ GameCloudScape/src/game/Screen.java
@@ -0,0 +1,48 @@
+package game;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Screen extends JFrame
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private WorldMap worldMap;
+	private RenderPanel renderPanel;
+	
+	public Screen(WorldMap worldMap)
+	{
+		JPanel panel = new JPanel();
+		this.setSize(960, 640);
+		getContentPane().add(panel, BorderLayout.CENTER);
+		
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		this.worldMap = worldMap;
+		this.addKeyListener(new NormalKeyListener());
+		this.setFocusable(true);
+		
+		renderPanel = new RenderPanel(this.worldMap);
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		getContentPane().add(renderPanel, BorderLayout.CENTER);
+		
+		this.setVisible(true);
+		
+		this.setFocusable(true);
+		
+		this.requestFocus();
+	}
+	
+	public void end()
+	{
+		this.dispose();
+		
+	}
+}
