e39bb1cf7f97941b0f9f39ae91d793242a473ff4
pplluumm <bunnydolphins_6@yahoo.com>
2015-10-10 07:32:06 -0700

    Created LoadButton Base

--- GameCloudScape/src/start/LoadButton.java
+++ GameCloudScape/src/start/LoadButton.java
@@ -1,8 +1,33 @@
 package start;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 
 public class LoadButton extends JButton
 {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public LoadButton()
+	{
+		this.setText("Load");
+		this.setFocusable(false);
+		this.addActionListener(new ActionListener()
+		{
+			
+			public void actionPerformed(ActionEvent e)
+			{
+				JFileChooser loadFile = new LoadFileChooser(new File("Saves"));
+				loadFile.showOpenDialog(null);
+			}
+		});
+	}
 	
 }
