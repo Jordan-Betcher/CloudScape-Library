8c0bbbef2f35f778bfebe207730937f7d0da633a
pplluumm <bunnydolphins_6@yahoo.com>
2015-10-10 07:25:58 -0700

    Programmed base of StartButton

--- GameCloudScape/src/start/StartButton.java
+++ GameCloudScape/src/start/StartButton.java
@@ -1,8 +1,31 @@
 package start;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 
 public class StartButton extends JButton
 {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private static final String text = "Start";
+	
+	public StartButton()
+	{
+		super(text);
+		this.addActionListener(new ActionListener()
+		{
+			
+			public void actionPerformed(ActionEvent e)
+			{
+				//TODO create GameScreen, start that
+			}
+		});
+	}
 	
 }
