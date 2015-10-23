74398c7815f80d406e54a785cff05a9331e290dd
pplluumm <bunnydolphins_6@yahoo.com>
2015-10-23 14:56:52 -0700

    Created a task. use reflection to create a method that will get children

---
+++ GameCloudScape/src/helper/Inheritance.java
@@ -0,0 +1,14 @@
+package helper;
+
+import java.util.ArrayList;
+
+public class Inheritance
+{
+	public static ArrayList<Class<? extends Object>> getChildren(Class<? extends Object> parent)
+	{
+		//TODO import the reflection library from:
+		//https://github.com/ronmamo/reflections/tree/master/src/main/java/org/reflections
+		//And use reflection to get and return the children
+		return null;
+	}
+}
