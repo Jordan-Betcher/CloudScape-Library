9d9c56b93eb8ba6d1bd27894600e588b75f1e5f1
Job <bunnydolphins_8@yahoo.com>
2015-10-28 21:49:16 -0700

    Worked towards mod loading

--- GameCloudScape/src/modloader/ModLoader.java
+++ GameCloudScape/src/modloader/ModLoader.java
@@ -1,11 +1,41 @@
 package modloader;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
 
 public class ModLoader
 {
-	private ArrayList<Mod> unloadedMods = new ArrayList<Mod>();
+	private ArrayList<String> unloadedModURL = new ArrayList<String>();
 	private ArrayList<Mod> queuedMods = new ArrayList<Mod>();
 	private ArrayList<Mod> loadedMods = new ArrayList<Mod>();
+	
+	/**
+	 * http://stackoverflow.com/questions/60764/how-should-i-load-jars-dynamically-at-runtime
+	 * 
+	 */
+	public void loadMod(String url) throws IOException
+	{
+		File f = new File(url);
+        URL u = f.toURI().toURL();
+		
+		URLClassLoader sysloader = (URLClassLoader) ClassLoader
+				.getSystemClassLoader();
+		Class sysclass = URLClassLoader.class;
+
+		try {
+			Method method = sysclass.getDeclaredMethod("addURL");
+			method.setAccessible(true);
+			method.invoke(sysloader, new Object[] { u });
+		} catch (Throwable t) {
+			t.printStackTrace();
+			throw new IOException(
+					"Error, could not add URL to system classloader");
+		}
+
+	}
 	
 }
