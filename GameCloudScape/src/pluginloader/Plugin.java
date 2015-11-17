4e9e00447deda454f3ede96ad5c131757edc874f
pplluumm <bunnydolphins_6@yahoo.com>
2015-11-17 15:45:33 -0800

    Created plugin loader

--- GameCloudScape/src/pluginloader/Plugin.java
+++ GameCloudScape/src/pluginloader/Plugin.java
@@ -1,12 +1,36 @@
-package modloader;
+package pluginloader;
 
 import java.util.ArrayList;
 
-public interface Mod
+public interface Plugin
 {
-	public String getName();
-	public ArrayList<String> getRequiredMods();
+	/**
+	 * @return the name of the plugin
+	 */
+	public String getPluginName();
 	
-	public void starting();
-	public void stopping();
+	/**
+	 * 
+	 * @return the name of the author
+	 */
+	public String getAuthorName();
+	
+	/**
+	 * What does your plugin add/do
+	 * 
+	 * @return is the descriptions of the Plugin
+	 */
+	public String getDescription();
+	
+	/**
+	 * 
+	 * @return the list of plugins that need to be loaded before this can be run
+	 */
+	public ArrayList<String> getRequiredPlugins();
+	
+	/**
+	 * The method that is called to start your plugin
+	 * Use this to add events, blocks, items etc...
+	 */
+	public void initialization();
 }
--- GameCloudScape/src/pluginloader/PluginLoader.java
+++ GameCloudScape/src/pluginloader/PluginLoader.java
@@ -1,208 +1,189 @@
-package modloader;
+package pluginloader;
 
-import helper.FileCheck;
-
-import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Method;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
-public class ModLoader
+/*
+ * Classes loaded will be overriden if a class with the same name is part of the core game
+ * 
+ * Multiple different classes will be loaded even if they have the same name
+ * 
+ * 
+ */
+
+public class PluginLoader
 {
-	private static String MODFOLDER = "Mods/";
 	
-	private static ArrayList<Mod> allMods = new ArrayList<Mod>();
-	private static ArrayList<Mod> notLoadedMods = new ArrayList<Mod>();
-	private static ArrayList<Mod> loadedMods = new ArrayList<Mod>();
+	private ArrayList<String> jarPaths = new ArrayList<String>();
+	private ArrayList<String> classPaths = new ArrayList<String>();
+	private ArrayList<Class> classes = new ArrayList<Class>();
+	private ArrayList<Plugin> plugins = new ArrayList<Plugin>();
 	
-	private static HashMap<Mod, File> modToDirectory = new HashMap<Mod, File>();
-	
-	public static void load()
-	{
-		
-	}
-	
-	public static void loadMods()
-	{
-		for(int i = 0; i < allMods.size() - 1; i++)
-		{
-			if(notLoadedMods.contains(allMods.get(i)))
-			{
-				try
-				{
-					loadMod(allMods.get(i));
-				}
-				catch (IOException e)
-				{
-					//Gets thrown when 
-					e.printStackTrace();
-				}
-			}
-		}
-	}
-	
-	private static boolean isAllreadyLoaded(ArrayList<String> names)
-	{
-		return contains(loadedMods, names);
-	}
-	
-	private static boolean isInNotLoadedYet(ArrayList<String> names)
-	{
-		return contains(notLoadedMods, names);
-	}
-	
-	private static boolean contains(ArrayList<Mod> mods, ArrayList<String> names)
-	{
-		return getModNames(mods).containsAll(names);
-	}
-	
-	private static ArrayList<String> getModNames(ArrayList<Mod> mods)
-	{
-		ArrayList<String> modNames = new ArrayList<String>();
-		
-		for(Mod mod : mods)
-		{
-			modNames.add(mod.getName());
-		}
-		
-		return modNames;
-	}
-	
-	public static void loadMod(Mod mod) throws IOException
-	{
-		ArrayList<String> requiredMods = mod.getRequiredMods();
-		
-		if(isAllreadyLoaded(requiredMods))
-		{
-			try
-			{
-				loadMod(modToDirectory.get(mod));
-				loadedMods.add(mod);
-				mod.starting();
-			}
-			catch (Throwable t)
-			{
-				t.printStackTrace();
-				throw new IOException("couldn't load mod: " + mod.getName());
-			}
-			
-		}
-		//TODO make it so that if part of the mods are already loaded
-		//, that it only loads the ones that aren't loaded
-		else if(isInNotLoadedYet(requiredMods))
-		{
-			for(String requiredMod : requiredMods)
-			{
-				//TODO make it so that if one of these mods throws an exception
-				//, that this mod doesn't load and it return;
-				// but if it doesn't throw any exceptions, then the mod does the
-				// thing 
-				try
-				{
-					loadMod(getModToLoad(requiredMod));
-					
-					try
-					{
-						loadMod(modToDirectory.get(mod));
-						loadedMods.add(mod);
-						mod.starting();
-					}
-					catch (Throwable t)
-					{
-						t.printStackTrace();
-						throw new IOException("couldn't load mod: " + mod.getName());
-					}
-				}
-				catch (Throwable t)
-				{
-					t.printStackTrace();
-					throw new IOException("couldn't load required mods, this mod: " + mod.getName());
-				}
-			}
-		}
-		
-		if( ! loadedMods.contains(mod))
-		{
-			System.out.println("Couldn't find required mods in unloadedMods" + mod.getName() + ". Which are:");
-			
-			for(String requireModName : requiredMods)
-			{
-				System.out.println(requireModName);
-			}
-			
-			throw new IOException("mod wasn't loaded: " + mod.getName());
-		}
-		
-		notLoadedMods.remove(mod);
-	}
-
-	private static Mod getModToLoad(String requiredMod)
-	{
-		Mod mod;
-		
-		for(Mod notLoadedMod : notLoadedMods)
-		{
-			if(notLoadedMod.getName().equals(requiredMod))
-			{
-				return notLoadedMod;
-			}
-		}
-		
-		return null;
-	}
-
 	/**
-	 * http://stackoverflow.com/questions/60764/how-should-i-load-jars-dynamically-at-runtime
-	 * @throws IOException 
+	 * If an exception is thrown there is something wrong with the jar or class,
+	 * I'm not knowledgable enough to tell you how to fix them
 	 * 
+	 * I also couldn't make it so you could pass in a generic interface in so the classes you are looking for
+	 * have to be plugins
+	 * 
+	 * @author pplluumm
+	 * @param Folder is the directory for the folder the jars are stored in (in eclipse it is on the same level as the src folder)
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
 	 */
-	public static void loadMod(File file) throws IOException
+	public PluginLoader(String Folder) throws IOException,
+			ClassNotFoundException, InstantiationException,
+			IllegalAccessException
 	{
-		URL url = getURL(file);
-		
-		URLClassLoader sysloader = (URLClassLoader) ClassLoader
-				.getSystemClassLoader();
-		Class systemclass = URLClassLoader.class;
-		
-		try
-		{
-			Method method = systemclass.getDeclaredMethod("addURL");
-			method.setAccessible(true);
-			method.invoke(sysloader, new Object[]{ url });
-		} 
-		catch (Throwable t)
-		{
-			t.printStackTrace();
-			throw new IOException(
-					"Error, could not add URL to system classloader");
-		}
-		
+		this.jarPaths = getJarPaths(Folder);
+		this.classes = getClasses(jarPaths);
+		this.plugins = getPluginClasses(classes);
 	}
 	
-	private static URL getURL(File file) throws MalformedURLException
+	
+	
+	private ArrayList<Plugin> getPluginClasses(ArrayList<Class> classes)
+			throws InstantiationException, IllegalAccessException
 	{
-		return file.toURI().toURL();
+		ArrayList<Plugin> plugins = new ArrayList<Plugin>();
 		
-	}
-
-	public static void loadMod(String directory) throws IOException
-	{
-		File file = new File(directory);
-		loadMod(file);
-		
+		for (Class c : classes)
+		{
+			Object test = c.newInstance();
+			if (test instanceof Plugin)
+			{
+				Plugin plugin = (Plugin) test;
+				plugins.add(plugin);
+			}
+		}
+		return plugins;
 	}
 	
-	private static void makeModFileExist()
+	private ArrayList<Class> getClasses(ArrayList<String> jarPaths)
+			throws ClassNotFoundException, InstantiationException,
+			IllegalAccessException, IOException
 	{
-
-		if( ! FileCheck.exists(MODFOLDER))
+		ArrayList<Class> classes = new ArrayList<Class>();
+		
+		for (String jarPath : jarPaths)
 		{
-			FileCheck.createFile(MODFOLDER);
+			classes.addAll(getClasses(jarPath));
 		}
+		
+		return classes;
+	}
+	
+	/**
+	 *  Copied from http://stackoverflow.com/questions/11016092/how-to-load-classes-at-runtime-from-a-folder-or-jar
+	 *  
+	 * @param pathToJar
+	 * @return
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 */
+	private ArrayList<Class> getClasses(String pathToJar) throws IOException,
+			ClassNotFoundException, InstantiationException,
+			IllegalAccessException
+	{
+		
+		ArrayList<Class> classes = new ArrayList<Class>();
+		
+		URL[] urls =
+		{
+			new URL("jar:file:" + pathToJar + "!/")
+		};
+		
+		URLClassLoader cl = URLClassLoader.newInstance(urls);
+		JarFile jarFile = new JarFile(pathToJar);
+		Enumeration<JarEntry> jars = jarFile.entries();
+		
+		while (jars.hasMoreElements())
+		{
+			JarEntry entry = (JarEntry) (jars.nextElement());
+			if (entry.isDirectory() || !entry.getName().endsWith(".class"))
+			{
+				continue;
+			}
+			String className = entry.getName().substring(0,
+					entry.getName().length() - 6);
+			className = className.replace('/', '.');
+			
+			classes.add(cl.loadClass(className));
+			this.classPaths.add(className);
+		}
+		
+		return classes;
+	}
+	
+	private ArrayList<String> getJarPaths(String folder) throws IOException
+	{
+		ArrayList<String> jarPaths = new ArrayList<String>();
+		
+		Files.walk(Paths.get(folder)).forEach(filePath ->
+		{
+			if (Files.isRegularFile(filePath))
+			{
+				jarPaths.add("" + filePath);
+			}
+		});
+		
+		return jarPaths;
+	}
+	
+	/**
+	 * Created at initialization by private method getJarPaths
+	 * 
+	 * @author pplluumm
+	 * @return the paths to each jar in the folder given at initialization
+	 */
+	public ArrayList<String> getJarPaths()
+	{
+		return jarPaths;
+	}
+	
+	/**
+	 * Created at initialization by private method getClasses
+	 * 
+	 * @author pplluumm
+	 * @return all the classes loaded
+	 */
+	public ArrayList<Class> getLoadedClasses()
+	{
+		return classes;
+	}
+	
+	/**
+	 * Created at initialization by private method getPluginClasses
+	 * 
+	 * @author pplluumm
+	 * @return all the class that are instances of Plugin that were found and loaded in the folder
+	 */
+	public ArrayList<Plugin> getPlugins()
+	{
+		return plugins;
+	}
+	
+	/**
+	 * Gets an arraylist of strings that represents the directory for where the class was found
+	 * You can use this to find out where a certain class is found
+	 * 
+	 * @author pplluumm
+	 * @return an arrayList of strings that are the directories for classes
+	 */
+	public ArrayList<String> getClassPaths()
+	{
+		return this.classPaths;
 	}
 	
 }
