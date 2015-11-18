4eec15e3d06d0533128e03eb79e64b71953a6201
pplluumm <bunnydolphins_6@yahoo.com>
2015-11-18 12:34:25 -0800

    Changed camara so that it is an interface instead of an abstract class

--- GameCloudScape/src/game/worldmap/camara/Camara.java
+++ GameCloudScape/src/game/worldmap/camara/Camara.java
@@ -1,41 +1,12 @@
 package game.worldmap.camara;
 
-import game.worldmap.Moveable;
-import game.worldmap.Position2D;
-import game.worldmap.Position3D;
 import game.worldmap.WorldMap;
 
 import java.awt.Graphics2D;
 
 
-public abstract class Camara implements Moveable
+public interface Camara
 {
-	
-	private WorldMap map;
-	private Position3D xyzPosition;
-	
-	public Camara(WorldMap map, Position3D xyzPosition)
-	{
-		this.map = map;
-		this.xyzPosition = xyzPosition;
-	}
-	
-	public Camara(WorldMap map, Position2D xyPosition)
-	{
-		this.map = map;
-		this.xyzPosition = new Position3D(xyPosition);
-	}
-	
-	@Override
-	public Position3D getPosition3D()
-	{
-		return xyzPosition;
-	}
-	
-	public void changeMap(WorldMap map)
-	{
-		this.map = map;
-	}
 
 	/**
 	 * Uses the Map to draw stuff 
@@ -44,10 +15,5 @@ public abstract class Camara implements Moveable
 	 * @param worldMap
 	 */
 	public abstract void draw(Graphics2D g2d, WorldMap worldMap);
-
-	public WorldMap getMap()
-	{
-		return map;
-	}
 	
 }
