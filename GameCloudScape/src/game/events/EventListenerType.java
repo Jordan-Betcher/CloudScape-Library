0bab33a5bf074c501c294f38140e0e55c876f02d
pplluumm <bunnydolphins_6@yahoo.com>
2015-12-03 15:39:29 -0800

    Got rid of auto create in EventListenerType. Was to confusing

--- GameCloudScape/src/game/events/EventListenerType.java
+++ GameCloudScape/src/game/events/EventListenerType.java
@@ -1,11 +1,6 @@
 package game.events;
 
 public abstract class EventListenerType<Information>
-{
-	public EventListenerType(Events events)
-	{
-		events.add(new Event<Information>());
-	}
-	
+{	
 	public abstract void activate(Information object);
 }
