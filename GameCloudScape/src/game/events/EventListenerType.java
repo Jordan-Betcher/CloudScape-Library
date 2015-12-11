be56828dd4c5d5ee6f734552297586faaf86b4a8
pplluumm <bunnydolphins_6@yahoo.com>
2015-12-10 17:00:56 -0800

    Modified the comment in EventListenerType to show more solutions

--- GameCloudScape/src/game/events/EventListenerType.java
+++ GameCloudScape/src/game/events/EventListenerType.java
@@ -2,14 +2,32 @@ package game.events;
 
 public interface EventListenerType<Information>
 {	
-	/*
+	/**
 	 * Unfortunately you won't be able to implement several listeners in the same class
 	 * (i.e. MouseEventListener (EventListenerType<MouseEvent>) and a KeyEventListener (EventListenerType<ArrayList<Character>))
 	 * Since Java doesn't deem it fit
 	 * 
 	 * So to have multiple listener (like if you are doing for a person mouse and keyboard events
-	 * and wanted to be able to access both in the same class) You will have to create to objects 
-	 * and when they are activated to tell the class you wanted (i.e. the player class)
-	 */
+	 * and wanted to be able to access both in the same class) 
+	 * public void activate( Object obj)
+	 *	{
+	 *		if(obj instanceof Character)
+	 *		{
+	 *			this.chars.add((Character) obj);
+	 *		}
+	 *		else if(obj instanceof MouseEvent)
+	 *		{
+	 *			this.mouseEvent = (MouseEvent) obj;
+	 *		}
+	 *		else
+	 *		{
+	 *			//Do nothing
+	 *		}
+	 *	} 
+	 *
+	 * OR 
+	 * 
+	 * create two classes that each use one of the interfaces and then when ever that is activated modify the player class
+	 **/
 	public void activate(Information object);
 }
