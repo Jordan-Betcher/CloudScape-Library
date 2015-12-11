a3e290e5d77d06c73a095972c4f128cfda2d01a2
pplluumm <bunnydolphins_6@yahoo.com>
2015-12-10 16:28:13 -0800

    Made comment about how Java doesn't allow you to implement several of the same interface even if they are generic with totally different types in them (i.e. you can implement both a KeyEventListener and a MouseEvent Listener)

--- GameCloudScape/src/game/events/EventListenerType.java
+++ GameCloudScape/src/game/events/EventListenerType.java
@@ -2,5 +2,14 @@ package game.events;
 
 public interface EventListenerType<Information>
 {	
+	/*
+	 * Unfortunately you won't be able to implement several listeners in the same class
+	 * (i.e. MouseEventListener (EventListenerType<MouseEvent>) and a KeyEventListener (EventListenerType<ArrayList<Character>))
+	 * Since Java doesn't deem it fit
+	 * 
+	 * So to have multiple listener (like if you are doing for a person mouse and keyboard events
+	 * and wanted to be able to access both in the same class) You will have to create to objects 
+	 * and when they are activated to tell the class you wanted (i.e. the player class)
+	 */
 	public void activate(Information object);
 }
